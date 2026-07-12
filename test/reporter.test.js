ec1f07182ca7aadb4f57321bb76fcecda365310c
Andre Meyer <amozaq@gmail.com>
2026-07-12 16:29:28 +1000

    option to run function before attempt is added

--- test/reporter.test.js
+++ test/reporter.test.js
@@ -250,6 +250,27 @@ test("attaches failure screenshots to the matching attempt", () => {
   assert.deepEqual(finalAttempt.meta.screenshot, { image: screenshotPath });
 });
 
+test("calls adjustAttempt when it's set", () => {
+  const projectRoot = tmpdir();
+  const outputFile = "results/rwx.json";
+  const runner = new EventEmitter();
+  const failing = createMochaTest({
+    titlePath: ["adjust"],
+    file: "cypress/e2e/adjust.cy.js",
+  });
+
+  new RwxReporter(runner, { reporterOptions: { outputFile, projectRoot, adjustAttempt: `${__dirname}/fixtures/adjustAttemptMock.js` } });
+
+  runner.emit("test", failing);
+  runner.emit("retry", failing, new Error("failure"));
+  runner.emit("end");
+
+  const results = readJson(path.join(projectRoot, outputFile));
+  const attempt = results.tests[0].attempt;
+
+  assert.equal(attempt.meta.html, 'fromAdjustAttempt-adjust-failed-failure-1');
+});
+
 test("uses hash output filenames", () => {
   const projectRoot = tmpdir();
   const runner = new EventEmitter();
